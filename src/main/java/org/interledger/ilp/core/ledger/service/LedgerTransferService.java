10bafc7e5e3ce9cc8756d017f2466c1f9e57f001
Andrew <andrew@appliedpayments.co.za>
2016-11-27 18:41:02 +0200

    add javadoc for send method

--- src/main/java/org/interledger/ilp/core/ledger/service/LedgerTransferService.java
+++ src/main/java/org/interledger/ilp/core/ledger/service/LedgerTransferService.java
@@ -4,6 +4,12 @@ import org.interledger.ilp.core.ledger.model.LedgerTransfer;
 
 public interface LedgerTransferService {
 
+  /**
+   * Initiates a ledger-local transfer.
+   *
+   * @param transfer
+   *          <code>LedgerTransfer</code>
+   */
   public void send(LedgerTransfer transfer) throws Exception;
 
 }
