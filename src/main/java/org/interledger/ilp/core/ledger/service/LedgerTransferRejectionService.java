f4f28c1e3a4cd369aead7190a5ac0122df30f66b
Andrew <andrew@appliedpayments.co.za>
2016-11-27 18:39:42 +0200

    define ledger transfer rejection service.

---
+++ src/main/java/org/interledger/ilp/core/ledger/service/LedgerTransferRejectionService.java
@@ -0,0 +1,16 @@
+package org.interledger.ilp.core.ledger.service;
+
+import org.interledger.ilp.core.ledger.model.LedgerTransfer;
+import org.interledger.ilp.core.ledger.model.TransferRejectedReason;
+
+public interface LedgerTransferRejectionService {
+  /**
+   * Reject a transfer
+   *
+   * This should only be allowed if the entity rejecting the transfer is the receiver
+   *
+   * @param transfer
+   * @param reason
+   */
+  void rejectTransfer(LedgerTransfer transfer, TransferRejectedReason reason);
+}
