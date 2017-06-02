d6ccccfc880e6dc4e6c8d2ecc1493b01612d6349
Andrew <andrew@appliedpayments.co.za>
2017-06-02 19:29:46 +0200

    fix: include changes not in last push

--- src/main/java/org/interledger/codecs/oer/OerUint128Codec.java
+++ src/main/java/org/interledger/codecs/oer/OerUint128Codec.java
@@ -2,7 +2,7 @@ package org.interledger.codecs.oer;
 
 import org.interledger.codecs.Codec;
 import org.interledger.codecs.CodecContext;
-import org.interledger.codecs.oer.OerUint64Codec.OerUint64;
+import org.interledger.codecs.oer.OerUint128Codec.OerUint128;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,18 +11,20 @@ import java.math.BigInteger;
 import java.util.Objects;
 
 /**
- * <p>An extension of {@link Codec} for reading and writing an ASN.1 OER 64-Bit unsigned integer 
+ * <p>An extension of {@link Codec} for reading and writing an ASN.1 OER 128-Bit unsigned integer 
  * type as defined by the Interledger ASN.1 definitions.</p>
- * <p>All Interledger ASN.1 integer types are encoded as fixed-size, non-extensible numbers. Thus,
- * for a UInt64 type, the integer value is encoded as an unsigned binary integer in 8 octets, and
- * supports values in the range (0..18446744073709551615). </p>
+ * <p>All Interledger ASN.1 integer types are encoded as fixed-size, non-extensible numbers.  Thus,
+ * for a UInt128 type, the integer value is encoded as an unsigned binary integer in sixteen octets,
+ * supporting values in the range (0..18446744073709551615). </p>
  */
-public class OerUint64Codec implements Codec<OerUint64> {
+public class OerUint128Codec implements Codec<OerUint128> {
 
+  public static final BigInteger MAX_VALUE = new BigInteger("18446744073709551615");
+  
   /**
-   * ASN.1 64BitUInt: If the lower bound of the value range constraint is not less than 0 and the
+   * ASN.1 128BitUInt: If the lower bound of the value range constraint is not less than 0 and the
    * upper bound is not greater than 18446744073709551615 and the constraint is not extensible,
-   * the integer value is encoded as an unsigned binary integer in eight octets.
+   * the integer value is encoded as an unsigned binary integer in sixteen octets.
    *
    * @param context     An instance of {@link CodecContext}.
    * @param inputStream An instance of @link InputStream}.
@@ -30,19 +32,19 @@ public class OerUint64Codec implements Codec<OerUint64> {
    * @throws IllegalArgumentException If the input has a value greater than 18446744073709551615.
    */
   @Override
-  public OerUint64 read(final CodecContext context, final InputStream inputStream)
+  public OerUint128 read(final CodecContext context, final InputStream inputStream)
       throws IOException {
     Objects.requireNonNull(context);
     Objects.requireNonNull(inputStream);
 
-    byte[] value = new byte[8];
+    byte[] value = new byte[16];
     int read = inputStream.read(value);
     
-    if (read != 8) {
-      throw new IOException("unexpected end of stream. expected 8 bytes, read " + read);
+    if (read != 16) {
+      throw new IOException("unexpected end of stream. expected 16 bytes, read " + read);
     }
     
-    return new OerUint64(new BigInteger(1, value));
+    return new OerUint128(new BigInteger(1, value));
   }
 
   /**
@@ -58,26 +60,32 @@ public class OerUint64Codec implements Codec<OerUint64> {
    */
   @Override
   public void write(
-      final CodecContext context, final OerUint64 instance, final OutputStream outputStream
+      final CodecContext context, final OerUint128 instance, final OutputStream outputStream
   ) throws IOException, IllegalArgumentException {
 
     Objects.requireNonNull(context);
     Objects.requireNonNull(instance);
     Objects.requireNonNull(outputStream);
 
-    
+    if (instance.getValue().compareTo(BigInteger.ZERO) < 0
+        || instance.getValue().compareTo(MAX_VALUE) > 1) {
+      throw new IllegalArgumentException(
+          "Interledger Uint128 only supports values from 0 to 18446744073709551615, value "
+              + instance.getValue() + " is out of range.");
+    }
+   
     byte[] value = instance.getValue().toByteArray();
     
-    /* BigInteger's toByteArray writes data in two's complement, so positive values requiring 64
+    /* BigInteger's toByteArray writes data in two's complement, so positive values requiring 128
      * bits will include a leading byte set to 0 which we don't want. */
-    if (value.length > 8) {
-      outputStream.write(value, value.length - 8, 8);
+    if (value.length > 16) {
+      outputStream.write(value, value.length - 16, 16);
       return;
     }
     
     /* BigInteger.toByteArray will return the smallest byte array possible. We are committed
      * to a fixed number of bytes, so we might need to pad the value out. */
-    for (int i = 0; i < 8 - value.length; i++) {
+    for (int i = 0; i < 16 - value.length; i++) {
       outputStream.write(0);
     }
     outputStream.write(value);
@@ -86,22 +94,11 @@ public class OerUint64Codec implements Codec<OerUint64> {
   /**
    * Merely a typing mechanism for registering multiple codecs that operate on the same type.
    */
-  public static class OerUint64 {
+  public static class OerUint128 {
 
     private final BigInteger value;
 
-    /**
-     * Constructs an OerUint64 instance.
-     * 
-     * @param value The value to read or write as an OER 64-bit int value.
-     * @deprecated OER Uint64 supports values beyond the range of Java long primitives
-     */
-    @Deprecated 
-    public OerUint64(final long value) {
-      this.value = new BigInteger(Long.toString(value));
-    }
-    
-    public OerUint64(final BigInteger value) {
+    public OerUint128(final BigInteger value) {
       this.value = value;
     }
 
@@ -118,9 +115,9 @@ public class OerUint64Codec implements Codec<OerUint64> {
         return false;
       }
 
-      OerUint64 oerUint64 = (OerUint64) obj;
+      OerUint128 oerUint128 = (OerUint128) obj;
 
-      return value.equals(oerUint64.value);
+      return value.equals(oerUint128.value);
     }
 
     @Override
@@ -130,7 +127,7 @@ public class OerUint64Codec implements Codec<OerUint64> {
 
     @Override
     public String toString() {
-      final StringBuilder sb = new StringBuilder("OerUint64{");
+      final StringBuilder sb = new StringBuilder("OerUint128{");
       sb.append("value=").append(value);
       sb.append('}');
       return sb.toString();
