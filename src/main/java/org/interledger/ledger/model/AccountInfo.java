9c4d9fad1518b405fa363d96065037e94a8701c7
Enrique Arizón Benito <enrique.arizon.benito@everis.com>
2017-09-01 07:25:52 +0000

    Avoid name colision with similar non-ILP related methods in existing libraries

--- src/main/java/org/interledger/ledger/model/AccountInfo.java
+++ src/main/java/org/interledger/ledger/model/AccountInfo.java
@@ -14,7 +14,7 @@ public interface AccountInfo {
   /**
    * Returns the ILP address of the ledger on which the account is held.
    */
-  InterledgerAddress getLedger();
+  InterledgerAddress getILPLedger();
 
   /**
    * Returns a unique identifier for the account with respect to the ledger.
@@ -35,7 +35,7 @@ public interface AccountInfo {
   /**
    * Returns the current balance of the account.
    */
-  MonetaryAmount getBalance();
+  MonetaryAmount getILPBalance();
 
   /**
    * Flag indicating if the account is currently able to transact.
@@ -52,7 +52,7 @@ public interface AccountInfo {
   /**
    * Returns the minimum allowed balance for the account, or null if there is no limit.
    */
-  MonetaryAmount getMinimumAllowedBalance();
+  MonetaryAmount getILPMinimumAllowedBalance();
 
   /**
    * Returns the public key for this account.
